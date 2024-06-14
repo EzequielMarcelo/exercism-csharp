3402676098b961e5f3653b2d7639edc5121272b4
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-06-13 22:55:44 -0300

    CHANGED: Log levels.

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -7,7 +7,7 @@ static class LogLine
     public static string Message(string logLine) => logLine.Split(':').LastOrDefault().Trim();
 
     public static string LogLevel(string logLine) => 
-        logLine.Substring(logLine.IndexOf('[') + 1, logLine.IndexOf(']') - 1).ToLower();
+        logLine.Substring(logLine.IndexOf('[') + 1, logLine.IndexOf(']') - logLine.IndexOf('[') - 1).ToLower();
 
     public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
 }
