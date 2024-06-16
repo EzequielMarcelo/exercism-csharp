aec96fb4b263cb3c0bd7b9420bc3e445ba62e779
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-06-15 23:48:15 -0300

    ADDED: Cars Assemble.

---
+++ cars-assemble/CarsAssemble.cs
@@ -0,0 +1,20 @@
+using System;
+
+static class AssemblyLine
+{
+    const int carsProducedPerHour = 221;
+
+    public static double SuccessRate(int speed) => speed switch
+    {
+        0 => 0.0,
+        >= 1 and <= 4 => 1.0,
+        >= 5 and <= 8 => 0.9,
+        9 => 0.8,
+        10 => 0.77,
+        _ => 0.0
+    };
+
+    public static double ProductionRatePerHour(int speed) => speed * carsProducedPerHour * SuccessRate(speed);
+
+    public static int WorkingItemsPerMinute(int speed) => (int)ProductionRatePerHour(speed) / 60;
+}
