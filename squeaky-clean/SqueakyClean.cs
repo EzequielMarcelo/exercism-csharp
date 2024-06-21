f5aa4914e00bcb3d6c6e7dd94c15e3beb8add0e1
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-06-20 22:27:15 -0300

    ADDED: Squeaky Clean.

---
+++ squeaky-clean/SqueakyClean.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+public static class Identifier
+{
+    public static string Clean(string identifier)
+    {
+        StringBuilder result = new();
+        bool convertNextToUpper = false;
+
+        foreach (char c in identifier)
+        {
+            if (char.IsControl(c))
+            {
+                result.Append("CTRL");
+            }
+            else if (c == ' ')
+            {
+                result.Append('_');
+            }
+            else if (c == '-')
+            {
+                convertNextToUpper = true;
+            }
+            else if (char.IsLetter(c))
+            {
+                if (c >= 'α' && c <= 'ω') 
+                {
+                    continue;
+                }
+
+                if (convertNextToUpper)
+                {
+                    result.Append(char.ToUpper(c));
+                    convertNextToUpper = false;
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+        }
+        return result.ToString();
+    }
+}
